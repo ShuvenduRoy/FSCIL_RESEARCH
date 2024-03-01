fe86abbc26a4a529d884fc9a4713da98cd8cd33a
Shuvendu Roy <bikash.shuvendu@gmail.com>
2024-03-01 16:51:20 -0500

    updating vit to fix bug

--- models/public/backbones/vit.py
+++ models/public/backbones/vit.py
@@ -29,18 +29,18 @@ Credit: https://raw.githubusercontent.com/rwightman/pytorch-image-models/v0.5.4/
 from functools import partial
 from typing import Any
 
+import numpy as np
 import torch
 from timm.data import (
     IMAGENET_INCEPTION_MEAN,
     IMAGENET_INCEPTION_STD,
 )
-from timm.models.helpers import build_model_with_cfg, named_apply
+from timm.models.helpers import adapt_input_conv, build_model_with_cfg, named_apply
 from timm.models.layers import PatchEmbed, trunc_normal_
 from timm.models.vision_transformer import (
     Block,
     _init_vit_weights,
-    _load_weights,
-    checkpoint_filter_fn,
+    resize_pos_embed,
 )
 from torch import nn
 
@@ -72,6 +72,152 @@ def _cfg(url: str = "", **kwargs: dict) -> dict:
         "first_conv": "patch_embed.proj",
         **kwargs,
     }
+
+
+def checkpoint_filter_fn(state_dict: Any, model: Any) -> dict:
+    """Convert patch embedding weight from manual patchify + linear proj to conv."""
+    out_dict = {}
+    if "model" in state_dict:
+        # For deit models
+        state_dict = state_dict["model"]
+    for k, v in state_dict.items():
+        # ignore head and pre_logits
+        if k.startswith("head.") or k.startswith("pre_logits"):
+            continue
+        if "patch_embed.proj.weight" in k and len(v.shape) < 4:
+            # For old models that I trained prior to conv based patchification
+            O, I, H, W = model.patch_embed.proj.weight.shape  # noqa: N806, E741
+            v = v.reshape(O, -1, H, W)  # noqa: PLW2901
+        elif k == "pos_embed" and v.shape != model.pos_embed.shape:
+            # To resize pos emb when using model at different size of pretrained weight
+            v = resize_pos_embed(  # noqa: PLW2901
+                v,
+                model.pos_embed,
+                getattr(model, "num_tokens", 1),
+                model.patch_embed.grid_size,
+            )
+        out_dict[k] = v
+    return out_dict
+
+
+@torch.no_grad()
+def _load_weights(  # noqa: PLR0915
+    model: Any,
+    checkpoint_path: str,
+    prefix: str = "",
+) -> None:
+    """Load weights from .npz checkpoints for Google Brain Flax implementation."""
+
+    def _n2p(w: Any, t: bool = True) -> torch.Tensor:
+        if w.ndim == 4 and w.shape[0] == w.shape[1] == w.shape[2] == 1:
+            w = w.flatten()
+        if t:
+            if w.ndim == 4:
+                w = w.transpose([3, 2, 0, 1])
+            elif w.ndim == 3:
+                w = w.transpose([2, 0, 1])
+            elif w.ndim == 2:
+                w = w.transpose([1, 0])
+        return torch.from_numpy(w)
+
+    w = np.load(checkpoint_path)
+    if not prefix and "opt/target/embedding/kernel" in w:
+        prefix = "opt/target/"
+
+    if hasattr(model.patch_embed, "backbone"):
+        # hybrid
+        backbone = model.patch_embed.backbone
+        stem_only = not hasattr(backbone, "stem")
+        stem = backbone if stem_only else backbone.stem
+        stem.conv.weight.copy_(
+            adapt_input_conv(
+                stem.conv.weight.shape[1],
+                _n2p(w[f"{prefix}conv_root/kernel"]),
+            ),
+        )
+        stem.norm.weight.copy_(_n2p(w[f"{prefix}gn_root/scale"]))
+        stem.norm.bias.copy_(_n2p(w[f"{prefix}gn_root/bias"]))
+        if not stem_only:
+            for i, stage in enumerate(backbone.stages):
+                for j, block in enumerate(stage.blocks):
+                    bp = f"{prefix}block{i + 1}/unit{j + 1}/"
+                    for r in range(3):
+                        getattr(block, f"conv{r + 1}").weight.copy_(
+                            _n2p(w[f"{bp}conv{r + 1}/kernel"]),
+                        )
+                        getattr(block, f"norm{r + 1}").weight.copy_(
+                            _n2p(w[f"{bp}gn{r + 1}/scale"]),
+                        )
+                        getattr(block, f"norm{r + 1}").bias.copy_(
+                            _n2p(w[f"{bp}gn{r + 1}/bias"]),
+                        )
+                    if block.downsample is not None:
+                        block.downsample.conv.weight.copy_(
+                            _n2p(w[f"{bp}conv_proj/kernel"]),
+                        )
+                        block.downsample.norm.weight.copy_(
+                            _n2p(w[f"{bp}gn_proj/scale"]),
+                        )
+                        block.downsample.norm.bias.copy_(
+                            _n2p(w[f"{bp}gn_proj/bias"]),
+                        )
+        embed_conv_w = _n2p(w[f"{prefix}embedding/kernel"])
+    else:
+        embed_conv_w = adapt_input_conv(
+            model.patch_embed.proj.weight.shape[1],
+            _n2p(w[f"{prefix}embedding/kernel"]),
+        )
+    model.patch_embed.proj.weight.copy_(embed_conv_w)
+    model.patch_embed.proj.bias.copy_(_n2p(w[f"{prefix}embedding/bias"]))
+    model.cls_token.copy_(_n2p(w[f"{prefix}cls"], t=False))
+    pos_embed_w = _n2p(
+        w[f"{prefix}Transformer/posembed_input/pos_embedding"],
+        t=False,
+    )
+    if pos_embed_w.shape != model.pos_embed.shape:
+        pos_embed_w = resize_pos_embed(  # resize pos embedding when different size from pretrained weights
+            pos_embed_w,
+            model.pos_embed,
+            getattr(model, "num_tokens", 1),
+            model.patch_embed.grid_size,
+        )
+    model.pos_embed.copy_(pos_embed_w)
+    model.norm.weight.copy_(_n2p(w[f"{prefix}Transformer/encoder_norm/scale"]))
+    model.norm.bias.copy_(_n2p(w[f"{prefix}Transformer/encoder_norm/bias"]))
+    for i, block in enumerate(model.blocks.children()):
+        block_prefix = f"{prefix}Transformer/encoderblock_{i}/"
+        mha_prefix = block_prefix + "MultiHeadDotProductAttention_1/"
+        block.norm1.weight.copy_(_n2p(w[f"{block_prefix}LayerNorm_0/scale"]))
+        block.norm1.bias.copy_(_n2p(w[f"{block_prefix}LayerNorm_0/bias"]))
+        block.attn.qkv.weight.copy_(
+            torch.cat(
+                [
+                    _n2p(w[f"{mha_prefix}{n}/kernel"], t=False).flatten(1).T
+                    for n in ("query", "key", "value")
+                ],
+            ),
+        )
+        block.attn.qkv.bias.copy_(
+            torch.cat(
+                [
+                    _n2p(w[f"{mha_prefix}{n}/bias"], t=False).reshape(-1)
+                    for n in ("query", "key", "value")
+                ],
+            ),
+        )
+        block.attn.proj.weight.copy_(
+            _n2p(w[f"{mha_prefix}out/kernel"]).flatten(1),
+        )
+        block.attn.proj.bias.copy_(_n2p(w[f"{mha_prefix}out/bias"]))
+        for r in range(2):
+            getattr(block.mlp, f"fc{r + 1}").weight.copy_(
+                _n2p(w[f"{block_prefix}MlpBlock_3/Dense_{r}/kernel"]),
+            )
+            getattr(block.mlp, f"fc{r + 1}").bias.copy_(
+                _n2p(w[f"{block_prefix}MlpBlock_3/Dense_{r}/bias"]),
+            )
+        block.norm2.weight.copy_(_n2p(w[f"{block_prefix}LayerNorm_2/scale"]))
+        block.norm2.bias.copy_(_n2p(w[f"{block_prefix}LayerNorm_2/bias"]))
 
 
 class VisionTransformer(nn.Module):
@@ -237,7 +383,7 @@ def _create_vision_transformer(
         ),
         default_cfg=default_cfg,
         pretrained_filter_fn=checkpoint_filter_fn,
-        pretrained_custom_load=False,
+        pretrained_custom_load=default_cfg["url"] and "npz" in default_cfg["url"],
         **kwargs,
     )
     if (
