1a972196aa60005382b7add6004b68c72dcb63a2
Shuvendu Roy <bikash.shuvendu@gmail.com>
2024-03-01 21:30:40 -0500

    removing choice for pre-trained url

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -228,14 +228,6 @@ def get_command_line_parser() -> argparse.ArgumentParser:  # noqa: PLR0915
         "--pre_trained_url",
         type=str,
         default=None,
-        choices=[
-            None,
-            "https://storage.googleapis.com/vit_models/imagenet21k/ViT-B_16.npz",
-            "./checkpoint/moco_v3.pth",
-            "./checkpoint/ibot_student.pth",
-            "./checkpoint/ibot_1k.pth",
-            "https://dl.fbaipublicfiles.com/dino/dino_vitbase16_pretrain/dino_vitbase16_pretrain.pth",
-        ],  # TODO need more options
         help="loading model parameter from local or remote url",
     )
 
